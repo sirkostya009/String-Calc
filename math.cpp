da22b944d53792131fd5fc603e01a41349499c62
Constantine <47304855+sirkostya009@users.noreply.github.com>
2020-01-31 20:43:37 +0200

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -1,8 +1,10 @@
 #include <string>
 
+// these funcs are being told about in comments of ids.cpp
 long getFirstInt(int j, std::string str);
 long getSecondInt(int j, std::string str);
 
+// gets two integers and divides them, simple
 long division(int j, std::string str) {
 	long first{ getFirstInt(j, str) };
 	long second{ getSecondInt(j, str) };
@@ -10,6 +12,7 @@ long division(int j, std::string str) {
 	return first / second;
 }
 
+// gets two integers and multiplies them
 long long multiplication(int j, std::string str) {
 	long first{ getFirstInt(j, str) };
 	long second{ getSecondInt(j, str) };
@@ -17,6 +20,7 @@ long long multiplication(int j, std::string str) {
 	return first * second;
 }
 
+// gets two integers and adds them
 long long addition(int j, std::string str) {
 	long first{ getFirstInt(j, str) };
 	long second{ getSecondInt(j, str) };
@@ -24,6 +28,7 @@ long long addition(int j, std::string str) {
 	return first + second;
 }
 
+// gets two integers and substracts them
 long substraction(int j, std::string str) {
 	long first{ getFirstInt(j, str) };
 	long second{ getSecondInt(j, str) };
