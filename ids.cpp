ba9b5464c0af52fb60294f06de5421c3b4e6e01c
Constantine <47304855+sirkostya009@users.noreply.github.com>
2020-01-31 20:41:24 +0200

    Update ids.cpp

--- ids.cpp
+++ ids.cpp
@@ -1,6 +1,11 @@
 #include <string>
 #include <cmath>
 
+/*
+* id1stNum_finder is a func that finds where first number begins to exist
+* example:
+* "11-4", first number here is 11, it begins from index 0 in string
+*/
 int id1stNum_finder(int j, std::string str) {
 	int id1stNum{ j };
 	while (true) {
@@ -13,7 +18,12 @@ int id1stNum_finder(int j, std::string str) {
 
 	return id1stNum;
 }
-
+/*
+* same goes to id2ndNum_finder
+* example:
+* "11-4", second number here is 4, but 2ndNum_finder seeks where it ends -
+* - in our case, it's 4, or length of our string;
+*/
 int id2ndNum_finder(int j, std::string str) {
 	int id2ndNum{ j };
 	while (true) {
@@ -24,6 +34,12 @@ int id2ndNum_finder(int j, std::string str) {
 	}
 }
 
+/*
+* basically what it does is getting first int, in case of str = "11-4" -
+* - it would return 11, where j is where the mathematical operator is located -
+* - index 2.
+* so, it parses all chars from indexes id1stNum (including) till the j.
+*/
 long getFirstInt(int j, std::string str) {
 	int id1stNum{ id1stNum_finder(j, str) };
 	long result{};
@@ -34,6 +50,10 @@ long getFirstInt(int j, std::string str) {
 	return result;
 }
 
+/*
+* this one does the same thing but in different order
+* it gets all the integers from 'j' (excluding it) till id2ndNum (excluding)
+*/
 long getSecondInt(int j, std::string str) {
 	int id2ndNum{ id2ndNum_finder(j, str) };
 
